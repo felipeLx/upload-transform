f943e6626607f30eb0f4b064df5c5dc2e09d722a
felipeLx <felipealvesfinanceiro@gmail.com>
2022-09-16 19:36:35 -0300

    function to clean and transformation

--- index.py
+++ index.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
-from google.cloud import firestore
 
 firebaseConfig = st.secrets['firebaseConfig']
           
